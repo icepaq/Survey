9a555362df67cb9bf08219d31f10bd9e6ea3d3c5
Anton <62949848+TheIcepack@users.noreply.github.com>
2020-06-26 08:08:47 -0400

    Project Initialization + Start
    Project created
    Created a welcome page with links to a French or English survey

---
+++ municipalsurvey/src/main/java/com/digitalcanada/municipalsurvey/MunicipalsurveyApplication.java
@@ -0,0 +1,13 @@
+package com.digitalcanada.municipalsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunicipalsurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunicipalsurveyApplication.class, args);
+	}
+
+}
---
+++ municipalsurvey/src/test/java/com/digitalcanada/municipalsurvey/MunicipalsurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalcanada.municipalsurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunicipalsurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
