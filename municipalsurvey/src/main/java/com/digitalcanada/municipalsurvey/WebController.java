eb08ed0dbab2f8797ae8452a8b4267ce8997629a
Anton <62949848+TheIcepack@users.noreply.github.com>
2020-06-27 11:33:19 -0400

    Feature Additions
    Added /english in Spring MVC and created english.html
    Fixed Javascript redirect issues
    Added comments

--- municipalsurvey/src/main/java/com/digitalcanada/municipalsurvey/WebController.java
+++ municipalsurvey/src/main/java/com/digitalcanada/municipalsurvey/WebController.java
@@ -6,10 +6,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 	
+	//Home Page
 	@RequestMapping(value = "/")
 	public String index() {
 		
 		return "index";
 	}
+	
+	//English Survey
+	@RequestMapping(value = "/english")
+	public String english() {
+		
+		return "english";
+	}
+	
+	
+	
 
 }
