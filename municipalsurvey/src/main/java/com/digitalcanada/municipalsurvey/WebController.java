72f95a0239ab1f5bbfa466736b6b5e9f5a7aeb71
Anton <62949848+TheIcepack@users.noreply.github.com>
2020-06-27 19:05:09 -0400

    Added Feature to Create Survey Questions
    Added Feature to Create Survey Questions

--- municipalsurvey/src/main/java/com/digitalcanada/municipalsurvey/WebController.java
+++ municipalsurvey/src/main/java/com/digitalcanada/municipalsurvey/WebController.java
@@ -1,7 +1,15 @@
 package com.digitalcanada.municipalsurvey;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class WebController {
@@ -20,7 +28,34 @@ public class WebController {
 		return "english";
 	}
 	
+	@RequestMapping(value = "/create_survey")
+	public String create_survey() {
+		
+		return "create_survey";
+	}
 	
-	
-
+	//Processing New Questions
+	@RequestMapping(value = "/submit_changes")
+	@ResponseBody
+	public String submit_changes(@RequestParam String question, @RequestParam String answer1, @RequestParam String answer2, @RequestParam String answer3, @RequestParam String answer4, @RequestParam String answer5, @RequestParam String answer6) throws SQLException {
+		
+		String query = "INSERT INTO survey_db.questions VALUES(NULL, 1, '" + question + "', " + "'" + answer1 + "|" + answer2 + "|"  + answer3 + "|" + answer4 + "|" + answer5 + "|" + answer6 + "');";
+		System.out.println(query);
+		
+		
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "spring_user", "Java_test.");
+		Statement stmt = conn.createStatement();
+		
+		try {
+			stmt.executeUpdate(query);
+		}catch(SQLException e) {
+			System.out.println(e);
+			return "SQL_ERROR";
+		} 
+		
+		
+			
+		return "SUCCESS";
+	}
 }
+	
